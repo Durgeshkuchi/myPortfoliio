4d592bfc9c2265a8e2fccffc6ad033b5fa35b060
Durgesh Kuchi <kuchid8144@uhcl.edu>
2023-10-04 17:11:38 -0400

    image-fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  logo = 'assets/kuchi.svg';
+  logo = './assets/kuchi.svg';
 }
